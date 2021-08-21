e00a8c350c07cf26e77f4ba1c1bd4ea089971db6
Mohamed Farook Mohamed Fazrin <mohamed.farzin@idsnext.com>
2021-08-21 18:20:46 +0530

    [HoldingCompanyID], [DefaultHoldingCompanyCode], [DefaultGroupID], [DefaultPropertyID],

---
+++ src/app/shared/INSERT INTO [dbo].sql
@@ -0,0 +1,15 @@
+INSERT INTO [dbo].[SSO_Users]
+([HoldingCompanyID], [DefaultHoldingCompanyCode], [DefaultGroupID], [DefaultPropertyID],
+[DefaultPMSCode], [UserType], [LoginID], [LoginPassword], [IsLocked], [IsFirstLogin],
+[RetryCount], [ValidFrom], [ValidTo], [ForcePasswordResetPeriod], [ForcePasswordReset],
+[DateFormat], [TimeFormat], [PreferredLanguage], [UTCOffset], [Title], [FirstName],
+[MiddleName], [LastName], [Gender], [DesignationID], [Designation], [DepartmentID],
+[Photo], [DateOfBirth], [OfficialMobile], [OfficialPhone], [OfficialExtention], [OfficialMailID],
+[OfficialAddress], [OfficialCountry], [OfficialState], [OfficialCity], [OfficialZip], [PersonalMobile],
+[PersonalPhone], [PersonalMailID], [PersonalAddress], [PersonalCountry], [PersonalState], [PersonalCity],
+[PersonalZip], [Remarks], [Status], [CreatedBy], [CreatedDateTime], [ModifiedBy], [ModifiedDateTime],
+[UpdateType], [DeletedBy], [IsDeleted], [IpAdress], [IpLog]) VALUES
+({0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},
+{18},{19},{20},{21},{22},{23},{24},{25},{26},{27},{28},{29},{30},{31},{32},{33},
+{34},{35},{36},{37},{38},{39},{40},{41},{42},{43},{44},{45},{46},
+{47},{48},{49},{50},{51},{52},{53},{54},{55},{56})
